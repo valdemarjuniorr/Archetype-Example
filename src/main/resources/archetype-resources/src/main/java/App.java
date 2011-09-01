22ec2194cfa5bf35938cea0ee69a17d648a7b0b1
Valdemar Jr <valdemar@valdemar-mec.(none)>
2011-09-01 11:58:16 -0300

    Basic structure do create maven archetypes

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.vjrcreative;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
